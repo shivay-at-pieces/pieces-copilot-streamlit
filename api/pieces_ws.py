867c44f0117594afc0d975db71607bd2376553a1
BishoyHanyRaafat <bishoyhany006@gmail.com>
2024-02-25 17:44:39 +0200

    ability to stream responses

--- api/pieces_ws.py
+++ api/pieces_ws.py
@@ -1,9 +1,12 @@
 import json
 import websocket
-import time
 import threading
+import pieces_os_client
+import queue
+import time
 WEBSOCKET_URL = "ws://localhost:1000/qgpt/stream"
-TIMEOUT = 60  # seconds
+TIMEOUT = 10  # seconds
+
 
 
 
@@ -15,39 +18,33 @@ class WebSocketManager:
         self.model_id = ""
         self.query = ""
         self.loading = False
-        self.last_message_time = None
         self.final_answer = ""
         self.open_event = threading.Event()  # wait for opening event
         self.conversation = None
+        self.message_queue = queue.Queue()
         #self.QGPTRelevanceInput = pieces_os_client.QGPTRelevanceInput(seeds=)
-        self.message_compeleted = threading.Event()
+        #self.message_compeleted = threading.Event()
         threading.Thread(target=self._start_ws).start()
         self.open_event.wait()
         
-
     def on_message(self,ws, message):
         """Handle incoming websocket messages."""
-        self.last_message_time = time.time()
-
         try:
-            response = json.loads(message)
-            
-            answers = response.get('question', {}).get('answers', {}).get('iterable', [])
+            response = pieces_os_client.QGPTStreamOutput.from_json(message)
+            if response.question:
+                answers = response.question.answers.iterable
+                for answer in answers:
+                    text = answer.text
+                    if text:
+                        self.message_queue.put(text)
+                        print(text, end='')
 
-            for answer in answers:
-                text = answer.get('text')
-                if text:
-                    print(text, end='')
-                    self.final_answer += " " + text
-
-            if response.get('status', '') == 'COMPLETED':
+            if response.status == 'COMPLETED':
                 print("\n")
-                self.conversation = response.get('conversation',None)
-                self.loading = False
-                self.message_compeleted.set()
-                
+                self.conversation = response.conversation
+                self.loading = False   # signal that the conversation is complete
 
-        except json.JSONDecodeError as e:
+        except Exception as e:
             print(f"Error processing message: {e}")
 
     def on_error(self, ws, error):
@@ -105,25 +102,39 @@ class WebSocketManager:
             self.ws.close()
             self.is_connected = False
 
-    def ask_question(self, model_id, query):
-        """Ask a question using the websocket."""
-        if self.loading:
-            return
+    def message_generator(self, model_id, query):
+        """
+        Stream messages from on message.
+        This function is a generator that yields messages as they become available.
+        """
+        
+        # Set loading to True and initialize model_id and query
         self.loading = True
         self.model_id = model_id
         self.query = query
-        self.response_received = None
-
+        
+        # Send the initial message
         self.send_message()
-
-        self.message_compeleted.wait(TIMEOUT)
-        self.message_compeleted = threading.Event() # creat new instance for another question
-        return self.final_answer
-
-# for testing purposes
-if __name__ == "__main__":
-    ws_m = WebSocketManager()
-    ws_m.ask_question("b3994dde-9458-4f02-a045-cdcb48bc3f0c", "What is the purpose of the Python programming language?")
-    print("=======================================================")
-    ws_m.ask_question("b3994dde-9458-4f02-a045-cdcb48bc3f0c", "What can python programming language do?")
-    ws_m.close_websocket_connection()
+        
+        # While loading is True, try to get messages from the queue
+        while self.loading:
+            try:
+                # If a message is available, yield it
+                yield self.message_queue.get(timeout=2)
+            except queue.Empty:
+                #endtime = time.time() + TIMEOUT
+                #while self.message_queue.empty():
+                    #remaining = endtime - time.time()
+                    #if remaining <= 0.0:
+                        #self.loading=False
+                        #raise ConnectionError("Timeout")
+                continue
+        
+        # Once loading is False, drain any remaining messages from the queue
+        while not self.message_queue.empty():
+            try:
+                # If a message is available, yield it
+                yield self.message_queue.get_nowait()
+            except queue.Empty:
+                # If the queue is empty, break the loop
+                break
--- pieces-bot.py
+++ pieces-bot.py
@@ -1,65 +1,46 @@
 import streamlit as st
 import pieces_os_client as pos_client
-import websocket
 from api import *
-final_answer = ""
 
+# Initialize variables
 pieces_os_version = None
-asset_ids = {} # Asset ids for any list or search
+asset_ids = {}  # Asset ids for any list or search
 assets_are_models = False
 current_asset = {}
 parser = None
-application = None
-ws = None # Websocket connection to pass to api.py when running
-ws_thread = None # Websocket thread to pass to api.py when running
-cli_version = None
-response_received = None
-existing_model_id = ""
 query = ""
-ws = websocket
-loading = False
-last_message_time = None
-initial_timeout = 10  # seconds
-subsequent_timeout = 5  # seconds
 
-configuration = pos_client.Configuration(host="http://localhost:1000")
+if "ws_manager" not in st.session_state:
+    st.session_state.ws_manager = WebSocketManager()
 
 # Initialize the ApiClient globally
+configuration = pos_client.Configuration(host="http://localhost:1000")
 api_client = pos_client.ApiClient(configuration)
-
-
-
 api_instance = pos_client.ModelsApi(api_client)
 
+# Get models
 api_response = api_instance.models_snapshot()
-models = {model.name: model.id for model in api_response.iterable if model.cloud or model.downloading} # getting the models that are available in the cloud or is downloaded
+models = {model.name: model.id for model in api_response.iterable if model.cloud or model.downloading}
 
+# Set default model
 default_model_name = "GPT-3.5-turbo Chat Model"
-model_id = models[default_model_name] # default model id
-models_name = [*models] # used in the option list
-default_model_index = models_name.index(default_model_name) # used in the option list
+model_id = models[default_model_name]
+models_name = list(models.keys())
+default_model_index = models_name.index(default_model_name)
 
-ws_manager = WebSocketManager()
 
-        
 
+# Streamlit UI
 st.title("Pieces Copilot Streamlit Bot")
 url = "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_43395aae44695b07e11c5cb6aa5bcc60/pieces-for-developers.png"
 st.image(url, caption="Pieces Copilot Streamlit Bot", use_column_width=True, width=10)
 
-
-
-selected_model = st.selectbox("Choose a model", index=default_model_index,options=models_name, key="dropdown")
+selected_model = st.selectbox("Choose a model", index=default_model_index, options=models_name, key="dropdown")
 model_id = models[selected_model]
 
 # Initialize chat history for Streamlit
 if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {
-            "role":"assistant",
-            "content":"Ask me Anything - Pieces Copilot"
-        }
-    ]
+    st.session_state.messages = [{"role": "assistant", "content": "Ask me Anything - Pieces Copilot"}]
 
 # Display chat messages from history on Pieces Bot app rerun
 for message in st.session_state.messages:
@@ -68,43 +49,24 @@ for message in st.session_state.messages:
 
 # Process and store Query and Response
 def pieces_copilot_function(query):
-    if ws_manager.loading:
-        with st.beta_expander("An error has occurred", expanded=True):
-            st.write(f"Details: {e}")
-        return
+    
+    with st.chat_message("user"): # Displaying the User Message
+        st.markdown(query)
     try:
-        final_answer = ws_manager.ask_question(model_id, query)
+        # Storing the User Message
+        st.session_state.messages.append({"role": "user", "content": query})
+        with st.chat_message("assistant"):
+            response = st.write_stream(st.session_state.ws_manager.message_generator(model_id, query))
+        st.session_state.messages.append({"role": "assistant", "content": response})
     except Exception as e:
         print(f"Error occurred while asking the question: {e}")
-    with st.chat_message("assistant"):
 
-        st.markdown(final_answer)
-
-        # Storing the User Message
-        st.session_state.messages.append(
-            {
-                "role": "user",
-                "content": query
-            }
-        )
-
-        # Storing the User Message
-        st.session_state.messages.append(
-            {
-                "role": "assistant",
-                "content": final_answer
-            }
-        )
-
-    # Displaying the Assistant Message
-    
 # Accept the user input
 query = st.chat_input("Ask a question to the Pieces Copilot")
 
 # Calling the Function when Input is Provided
 if query:
-    # Displaying the User Message
-    with st.chat_message("user"):
-        st.markdown(query)
-
-    pieces_copilot_function(query)
+    if st.session_state.ws_manager.loading:
+        st.warning('Hold on there is already a response generating', icon="⚠️")
+    else:
+        pieces_copilot_function(query)
